cbf3ef7187cd39faa9844f90cd062b1a202c17b3
Umar-FM <48263739+Umar-FM@users.noreply.github.com>
2021-10-22 08:46:08 -0400

    Time: 230 ms (22.98%), Space: 16.7 MB (60.55%) - LeetHub

--- single-number/single-number.py
+++ single-number/single-number.py
@@ -1,9 +1,8 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        dic = {}
+        res = 0 #0 XOR with anything is that thing
         for i in nums:
-            if i in dic.keys(): dic[i] += 1
-            else: dic[i] = 1
-            
-        return list(dic.keys())[list(dic.values()).index(1)]
+            res ^= i
+        
+        return res
         
