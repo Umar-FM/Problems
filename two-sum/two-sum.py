8a2d4a39f2c981c366918de5c0c365cd8f03799d
Umar-FM <48263739+Umar-FM@users.noreply.github.com>
2021-11-12 09:49:45 -0500

    Time: 60 ms (80.56%), Space: 15.7 MB (8.80%) - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -2,8 +2,6 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         d = {}
         for i,n in enumerate(nums):
-            m=target-n
-            if m in d:
-                return [d[m],i]
-            else:
-                d[n] = i
+            
+            if n in d: return [i,d[n]]
+            d[target-n]=i
